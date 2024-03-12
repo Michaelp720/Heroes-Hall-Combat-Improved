1bedddcda29c96328f29ab5df19f82d8a1ed5623
MichaelPile <mjpile@gmail.com>
2024-03-12 12:34:16 -0700

    sets up routing

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,28 @@
+// import React from "react";
+// import App from "./components/App";
+// import "./index.css";
+// import { createRoot } from "react-dom/client";
+
+// const container = document.getElementById("root");
+// const root = createRoot(container);
+// root.render(<App />);
+
+
 import React from "react";
-import App from "./components/App";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./components/Home";
+import VenturesContainer from './containers/VenturesContainer'
 import "./index.css";
 import { createRoot } from "react-dom/client";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<App />);
+
+root.render(
+  <Router>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/ventures" component={VenturesContainer} />
+    </Switch>
+  </Router>
+);
