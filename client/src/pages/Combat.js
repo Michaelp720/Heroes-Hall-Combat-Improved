279c7264a5814aa6f4f32c1c6ce06b3db7bd964a
MichaelPile <mjpile@gmail.com>
2024-03-18 11:23:08 -0700

    adds card group to combat

--- client/src/pages/Combat.js
+++ client/src/pages/Combat.js
@@ -4,8 +4,34 @@ import CharacterContainer from "../containers/CharacterContainer"
 import {PTurnContext} from '../context/playerturn'
 import StatusesContainer from "../containers/StatusesContainer"
 import {CombatContext} from '../context/combat'
-import { Button, Segment, Header } from 'semantic-ui-react'
+import { Button, Segment, Header, CardMeta,
+    CardHeader,
+    CardDescription,
+    CardContent,
+    Card,
+    Icon,
+    Image, CardGroup} from 'semantic-ui-react'
 import { useNavigate } from "react-router-dom";
+
+    <Card>
+      <Image src='/images/avatar/large/matthew.png' wrapped ui={false} />
+      <CardContent>
+        <CardHeader>Matthew</CardHeader>
+        <CardMeta>
+          <span className='date'>Joined in 2015</span>
+        </CardMeta>
+        <CardDescription>
+          Matthew is a musician living in Nashville.
+        </CardDescription>
+      </CardContent>
+      <CardContent extra>
+        <a>
+          <Icon name='user' />
+          22 Friends
+        </a>
+      </CardContent>
+    </Card>
+
 
 
 function Combat() {
@@ -31,24 +57,37 @@ function Combat() {
 
     if (pturn) {
         return (
-            <div>
-                <RoundCard/>
-                <StatusesContainer character={combat.player}/>
-                <CharacterContainer character={combat.player}/>
-                <StatusesContainer character={combat.enemy}/>
-                <CharacterContainer character={combat.enemy}/>
-            </div>
+            <Segment>
+            <RoundCard/>
+            <CardGroup itemsPerRow={3}>
+                <Card>
+                    <StatusesContainer character={combat.player}/>
+                    <CharacterContainer character={combat.player}/>
+                </Card>
+                <Button>Choose A Technique</Button>
+                <Card>
+                    <StatusesContainer character={combat.enemy}/>
+                    <CharacterContainer character={combat.enemy}/>
+                </Card>
+            </CardGroup>
+            </Segment>
         );
     } else {
         return (
-            <div>
-                <RoundCard/>
+            <Segment>
+            <RoundCard/>
+            <CardGroup itemsPerRow={3}>
+                <Card>
+                    <StatusesContainer character={combat.player}/>
+                    <CharacterContainer character={combat.player}/>
+                </Card>
                 <Button onClick={enemyAction}>Monster Action</Button>
-                <StatusesContainer character={combat.player}/>
-                <CharacterContainer character={combat.player}/>
-                <StatusesContainer character={combat.enemy}/>
-                <CharacterContainer character={combat.enemy}/>
-            </div>
+                <Card>
+                    <StatusesContainer character={combat.enemy}/>
+                    <CharacterContainer character={combat.enemy}/>
+                </Card>
+            </CardGroup>
+            </Segment>
         //round number
         //CharacterContainer passing combat.player as prop 
         //StatusesContainer passing combat.player
