4d586c330f38011d9c706dd33041422ec755db88
MichaelPile <mjpile@gmail.com>
2024-05-09 15:24:56 -0400

    adds Technique examples and effect class

--- server/effects.py
+++ server/effects.py
@@ -1,6 +1,57 @@
 
 
 class Effect:
-    def __init__(self, type, status):
-        self.type = type
-        self.status = status
+    def __init__(self, description, amnt_dur, chance = False, mod = 1, status = False):
+        self.description = description
+        self.amnt_dur = amnt_dur
+        self.chance = chance
+        self.mod = mod
+        self.status = status #dot, paired, other, stat change
+
+    #for calculating effects that use pwr
+    def calculate_amnt(self, pwr):
+        return self.mod * pwr + amnt_dur
+        
+
+
+#example- firebolt
+
+Technique(
+    name = "Firebolt",
+    hero_class = "Mage",
+    position = "432",
+    range = "234",
+    num_targets = 1,
+    target_type = "choice",
+    effects = [
+        Effect("Dmg", -2),
+        Effect("Brn", -3, False, 1, "dot")
+     ]
+),
+
+Technique(
+    name = "Flame Splash",
+    hero_class = "Mage",
+    position = "43",
+    range = "34",
+    num_targets = 2,
+    target_type = "adj",
+    effects = [
+        Effect("Dmg", -3),
+        Effect("Brn", -3, False, 1, "dot")
+    ]
+),
+
+Technique(
+    name = "Conflagration",
+    hero_class = "Mage",
+    position = "1",
+    range = "1",
+    num_targets = 2,
+    target_type = "adj",
+    effects = [
+        Effect("Back", 2),
+        Effect("Dmg", 4),
+        Effect("Knockback", 1, 4)
+    ]
+),
--- server/models.py
+++ server/models.py
@@ -1,168 +1,168 @@
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import validates
-from sqlalchemy.ext.associationproxy import association_proxy
-from sqlalchemy_serializer import SerializerMixin
+# from flask_sqlalchemy import SQLAlchemy
+# from sqlalchemy.orm import validates
+# from sqlalchemy.ext.associationproxy import association_proxy
+# from sqlalchemy_serializer import SerializerMixin
 
-from config import db
-
-
-class Combat(db.Model, SerializerMixin):
-    __tablename__ = 'combats'
-
-    id = db.Column(db.Integer, primary_key=True)
-    player_id = db.Column(db.Integer, db.ForeignKey('player.id'))
-    enemy_id = db.Column(db.Integer, db.ForeignKey('enemy.id'))
-    rnd = db.Column(db.Integer)
-    turn = db.Column(db.Integer) #1-8
-    player_next = db.Column(db.Boolean)
-    enemy_action = db.Column(db.String)
-    victor = db.Column(db.String)
-    # add relationship
-    statuses = db.relationship('Status', back_populates = 'combat')
-    player = db.relationship('Player', back_populates = 'combat')
-    enemy = db.relationship('Enemy', back_populates = 'combat')
-    # Add serialization rules
-    serialize_rules = ('-statuses.combat', '-player.combat', '-enemy.combat', '-player.statuses', '-enemy.statuses', '-player.known_techs', '-enemy.known_techs' )
-
-    def __repr__(self):
-        return f'<Hero {self.id}>'
-
-class Status(db.Model, SerializerMixin):
-    __tablename__ = 'statuses'
-
-    id = db.Column(db.Integer, primary_key=True)
-    remaining_duration = db.Column(db.Integer)
-    affected_stat = db.Column(db.String)
-    amnt = db.Column(db.Integer)
-
-    #foreign keys
-    combat_id = db.Column(db.Integer, db.ForeignKey('combats.id'))
-    character_id = db.Column(db.Integer, db.ForeignKey('characters.id'))
-
-    # relationships
-    combat = db.relationship('Combat', back_populates = 'statuses')
-    character = db.relationship('Character', back_populates = 'statuses')
-    # serialization rules
-    serialize_rules = ('-combat.statuses', '-character.statuses', '-combat.player', '-combat.enemy', '-character.combat', '-character.known_techs' )
-
-    # add validation
-    # @validates('affected_stat')
-    # def validate_affected_stat(self, key, value):
-    #     stats = [] #List of possible stats
-    #     if value not in stats:
-    #         raise ValueError("error message")
-    #     return value
-
-class Character(db.Model, SerializerMixin):
-    __tablename__ = 'characters'
-
-    id = db.Column(db.Integer, primary_key=True) 
-    name = db.Column(db.String, unique=True)
-    type = db.Column(db.String)
-    portrait = db.Column(db.String)
-
-    #base stat block
-    max_hp = db.Column(db.Integer)
-    base_pwr = db.Column(db.Integer)
-    base_def = db.Column(db.Integer)
-    spd = db.Column(db.Integer)
-
-    #combat stat block
-    crnt_hp = db.Column(db.Integer)
-    temp_pwr = db.Column(db.Integer)
-    temp_def = db.Column(db.Integer)
-    order = db.Column(db.Integer)
-
-    # add relationships
-    statuses = db.relationship('Status', back_populates = 'character')
-    known_techs = db.relationship('KnownTech', back_populates = 'character')
-
-    # Add serialization rules
-    serialize_rules = ('-statuses.character', '-known_techs.character')
-
-    __mapper_args__ = {
-        'polymorphic_identity': 'character',
-        'polymorphic_on': 'type'
-    }
-
-class Player(Character):
-    __tablename__ = 'player'
-    id = db.Column(db.Integer, db.ForeignKey('characters.id'), primary_key=True)
-    adv_points = db.Column(db.Integer)
+# from config import db
 
 
-    #relationships
-    combat = db.relationship('Combat', uselist=False, back_populates='player')
+# class Combat(db.Model, SerializerMixin):
+#     __tablename__ = 'combats'
 
-    # Add serialization rules
-    serialize_rules = ('-combat.player', )
+#     id = db.Column(db.Integer, primary_key=True)
+#     player_id = db.Column(db.Integer, db.ForeignKey('player.id'))
+#     enemy_id = db.Column(db.Integer, db.ForeignKey('enemy.id'))
+#     rnd = db.Column(db.Integer)
+#     turn = db.Column(db.Integer) #1-8
+#     player_next = db.Column(db.Boolean)
+#     enemy_action = db.Column(db.String)
+#     victor = db.Column(db.String)
+#     # add relationship
+#     statuses = db.relationship('Status', back_populates = 'combat')
+#     player = db.relationship('Player', back_populates = 'combat')
+#     enemy = db.relationship('Enemy', back_populates = 'combat')
+#     # Add serialization rules
+#     serialize_rules = ('-statuses.combat', '-player.combat', '-enemy.combat', '-player.statuses', '-enemy.statuses', '-player.known_techs', '-enemy.known_techs' )
 
-    __mapper_args__ = {
-        'polymorphic_identity': 'player'
-    }
+#     def __repr__(self):
+#         return f'<Hero {self.id}>'
 
-class Enemy(Character):
-    __tablename__ = 'enemy'
-    id = db.Column(db.Integer, db.ForeignKey('characters.id'), primary_key=True)
-    actions = db.Column(db.String)
-    threat_rating = db.Column(db.Integer)
+# class Status(db.Model, SerializerMixin):
+#     __tablename__ = 'statuses'
 
-    #crnt_action = db.Column(db.Integer)
+#     id = db.Column(db.Integer, primary_key=True)
+#     remaining_duration = db.Column(db.Integer)
+#     affected_stat = db.Column(db.String)
+#     amnt = db.Column(db.Integer)
+
+#     #foreign keys
+#     combat_id = db.Column(db.Integer, db.ForeignKey('combats.id'))
+#     character_id = db.Column(db.Integer, db.ForeignKey('characters.id'))
+
+#     # relationships
+#     combat = db.relationship('Combat', back_populates = 'statuses')
+#     character = db.relationship('Character', back_populates = 'statuses')
+#     # serialization rules
+#     serialize_rules = ('-combat.statuses', '-character.statuses', '-combat.player', '-combat.enemy', '-character.combat', '-character.known_techs' )
+
+#     # add validation
+#     # @validates('affected_stat')
+#     # def validate_affected_stat(self, key, value):
+#     #     stats = [] #List of possible stats
+#     #     if value not in stats:
+#     #         raise ValueError("error message")
+#     #     return value
+
+# class Character(db.Model, SerializerMixin):
+#     __tablename__ = 'characters'
+
+#     id = db.Column(db.Integer, primary_key=True) 
+#     name = db.Column(db.String, unique=True)
+#     type = db.Column(db.String)
+#     portrait = db.Column(db.String)
+
+#     #base stat block
+#     max_hp = db.Column(db.Integer)
+#     base_pwr = db.Column(db.Integer)
+#     base_def = db.Column(db.Integer)
+#     spd = db.Column(db.Integer)
+
+#     #combat stat block
+#     crnt_hp = db.Column(db.Integer)
+#     temp_pwr = db.Column(db.Integer)
+#     temp_def = db.Column(db.Integer)
+#     order = db.Column(db.Integer)
+
+#     # add relationships
+#     statuses = db.relationship('Status', back_populates = 'character')
+#     known_techs = db.relationship('KnownTech', back_populates = 'character')
+
+#     # Add serialization rules
+#     serialize_rules = ('-statuses.character', '-known_techs.character')
+
+#     __mapper_args__ = {
+#         'polymorphic_identity': 'character',
+#         'polymorphic_on': 'type'
+#     }
+
+# class Player(Character):
+#     __tablename__ = 'player'
+#     id = db.Column(db.Integer, db.ForeignKey('characters.id'), primary_key=True)
+#     adv_points = db.Column(db.Integer)
 
 
-    #relationships
-    combat = db.relationship('Combat', uselist=False, back_populates='enemy')
+#     #relationships
+#     combat = db.relationship('Combat', uselist=False, back_populates='player')
 
-    # Add serialization rules
-    serialize_rules = ('-combat.enemy', )
+#     # Add serialization rules
+#     serialize_rules = ('-combat.player', )
 
-    __mapper_args__ = {
-        'polymorphic_identity': 'enemy'
-    }
+#     __mapper_args__ = {
+#         'polymorphic_identity': 'player'
+#     }
 
-class KnownTech(db.Model, SerializerMixin):
-    __tablename__ = 'known_techs'
+# class Enemy(Character):
+#     __tablename__ = 'enemy'
+#     id = db.Column(db.Integer, db.ForeignKey('characters.id'), primary_key=True)
+#     actions = db.Column(db.String)
+#     threat_rating = db.Column(db.Integer)
 
-    id = db.Column(db.Integer, primary_key=True)
-    slot = db.Column(db.Integer)
-    rnk = db.Column(db.Integer)
+#     #crnt_action = db.Column(db.Integer)
 
-    #foreign keys
-    character_id = db.Column(db.Integer, db.ForeignKey('characters.id'))
-    tech_id = db.Column(db.Integer, db.ForeignKey('techs.id'))
 
-    # relationships
-    character = db.relationship('Character', back_populates = 'known_techs')
-    tech = db.relationship('Technique', back_populates = 'known_techs')
-    # serialization rules
-    serialize_rules = ('-character.known_techs', '-tech.known_techs' )
+#     #relationships
+#     combat = db.relationship('Combat', uselist=False, back_populates='enemy')
 
-class Technique(db.Model, SerializerMixin):
-    __tablename__ = 'techs'
+#     # Add serialization rules
+#     serialize_rules = ('-combat.enemy', )
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    target = db.Column(db.String)
-    duration = db.Column(db.Integer)
-    stat = db.Column(db.String)
-    modifier = db.Column(db.Integer)
-    amnt = db.Column(db.Integer)
-    unlocked = db.Column(db.Boolean, default = False)
+#     __mapper_args__ = {
+#         'polymorphic_identity': 'enemy'
+#     }
 
-    # relationships
-    known_techs = db.relationship('KnownTech', back_populates = 'tech')
-    # Add serialization rules
-    serialize_rules = ('-known_techs.tech', )
+# class KnownTech(db.Model, SerializerMixin):
+#     __tablename__ = 'known_techs'
 
-    @validates('target')
-    def validate_description(self, key, value):
-        if value != 'self' and value != 'opponent':
-            raise ValueError("Technique must target 'self' or 'opponent'")
-        return value
+#     id = db.Column(db.Integer, primary_key=True)
+#     slot = db.Column(db.Integer)
+#     rnk = db.Column(db.Integer)
 
-    @validates('stat')
-    def validate_description(self, key, value):
-        possible_stats = ["hp", "pwr", "def", "order"]
-        if value not in possible_stats:
-            raise ValueError("Technique must affect a stat")
-        return value
+#     #foreign keys
+#     character_id = db.Column(db.Integer, db.ForeignKey('characters.id'))
+#     tech_id = db.Column(db.Integer, db.ForeignKey('techs.id'))
+
+#     # relationships
+#     character = db.relationship('Character', back_populates = 'known_techs')
+#     tech = db.relationship('Technique', back_populates = 'known_techs')
+#     # serialization rules
+#     serialize_rules = ('-character.known_techs', '-tech.known_techs' )
+
+# class Technique(db.Model, SerializerMixin):
+#     __tablename__ = 'techs'
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String)
+#     target = db.Column(db.String)
+#     duration = db.Column(db.Integer)
+#     stat = db.Column(db.String)
+#     modifier = db.Column(db.Integer)
+#     amnt = db.Column(db.Integer)
+#     unlocked = db.Column(db.Boolean, default = False)
+
+#     # relationships
+#     known_techs = db.relationship('KnownTech', back_populates = 'tech')
+#     # Add serialization rules
+#     serialize_rules = ('-known_techs.tech', )
+
+#     @validates('target')
+#     def validate_description(self, key, value):
+#         if value != 'self' and value != 'opponent':
+#             raise ValueError("Technique must target 'self' or 'opponent'")
+#         return value
+
+#     @validates('stat')
+#     def validate_description(self, key, value):
+#         possible_stats = ["hp", "pwr", "def", "order"]
+#         if value not in possible_stats:
+#             raise ValueError("Technique must affect a stat")
+#         return value
